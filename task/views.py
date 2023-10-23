b65092e92785fbb3242a09d2a19c8b46bbba354b
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-10-23 14:46:19 -0600

    Completar y eliminar tarea

--- task/views.py
+++ task/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 # Create your views here.
 
 def inicio(request):
@@ -58,6 +59,17 @@ def task_detail(request, task_id):
             'error': 'Error al actualizar la tarea'
             })
 
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
 def create_task(request):
     if request.method == 'GET':
